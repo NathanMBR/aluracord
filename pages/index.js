810e06a3b04378055400f16888d4a0810ab0b3f7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-01-27 12:43:48 -0300

    chore: add dynamic html at components

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,37 @@
-const Title = props => (
-	<>
-		<h1>
-			{props.children}
-		</h1>
-		<style jsx>{`
-			h1 {
-				display: block;
-				padding: 5px;
-				text-align: center;
-				background-color: #222;
-				color: #fff;
-				border-radius: 5px;
-				box-shadow: 2px 2px 5px #333;
-			}
-		`}</style>
-	</>
-)
+const Title = props => {
+	/* Functionalities */
+	const Tag = props.Tag;
+
+	return (
+		<>
+			{/* Elements */}
+			<Tag>
+				{props.children}
+			</Tag>
+
+			{/* Style */}
+			<style jsx>{`
+				${Tag} {
+					display: block;
+					padding: 5px;
+					text-align: center;
+					background-color: #222;
+					color: #fff;
+					border-radius: 5px;
+					box-shadow: 2px 2px 5px #333;
+				}
+			`}</style>
+		</>
+	)
+}
 
 const log = msg => console.log(msg);
 
 export default () => (
 	<div>
-		<Title>Hello, world! Welcome to React.js + Next.js!</Title>
-		<Title>This is a Title.</Title>
-		<h1>This isn't a Title (at least, not a Title component)</h1>
+		<Title Tag="h1">Hello, world! Welcome to React.js + Next.js!</Title>
+		<Title Tag="h2">This is a Title.</Title>
+		<h2>This isn't a Title (at least, not a Title component)</h2>
 		<button onClick={() => log(document.querySelector("button"))}>Click me</button>
 	</div>
 );
