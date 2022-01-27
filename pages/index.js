90ccb9e8302d8f71d7452030c8cb53dd037d098a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-01-27 16:13:56 -0300

    chore: add global styles

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
+import appConfig from "../configs.json"
+
+const GlobalStyle = () => {
+	return (
+		<style global jsx>{`
+			* {
+				margin: 0;
+				padding: 0;
+				box-sizing: border-box;
+				list-style: none;
+			}
+
+			body {
+				font-family: 'Open Sans', sans-serif;
+			}
+
+			html, body, #__next {
+				min-height: 100vh;
+				display: flex;
+				flex: 1;
+			}
+
+			#__next {
+				flex: 1;
+			}
+
+			#__next > * {
+				flex: 1;
+			}
+		`}</style>
+	);
+}
+
 const Title = props => {
 	/* Functionalities */
 	const Tag = props.Tag;
@@ -16,7 +49,7 @@ const Title = props => {
 					padding: 5px;
 					text-align: center;
 					background-color: #222;
-					color: #fff;
+					color: ${appConfig.theme.colors.neutrals["000"]};
 					border-radius: 5px;
 					box-shadow: 2px 2px 5px #333;
 				}
@@ -25,13 +58,11 @@ const Title = props => {
 	)
 }
 
-const log = msg => console.log(msg);
-
 export default () => (
 	<div>
+		<GlobalStyle />
 		<Title Tag="h1">Hello, world! Welcome to React.js + Next.js!</Title>
 		<Title Tag="h2">This is a Title.</Title>
 		<h2>This isn't a Title (at least, not a Title component)</h2>
-		<button onClick={() => log(document.querySelector("button"))}>Click me</button>
 	</div>
 );
