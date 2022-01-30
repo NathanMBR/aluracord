5efd05ddac82bce5773144756d3c88934143ad79
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-01-30 11:28:23 -0300

    chore: add global styles to all next pages

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,139 @@
 import appConfig from "../configs.json"
+import { Box, Button, Text, TextField, Image } from "@skynexui/components"
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-const GlobalStyle = () => {
+function Title(props) {
+	const Tag = props.tag || 'h1';
 	return (
-		<style global jsx>{`
-			* {
-				margin: 0;
-				padding: 0;
-				box-sizing: border-box;
-				list-style: none;
-			}
-
-			body {
-				font-family: 'Open Sans', sans-serif;
-			}
-
-			html, body, #__next {
-				min-height: 100vh;
-				display: flex;
-				flex: 1;
-			}
-
-			#__next {
-				flex: 1;
-			}
-
-			#__next > * {
-				flex: 1;
-			}
-		`}</style>
+	  <>
+		<Tag>{props.children}</Tag>
+		<style jsx>{`
+			  ${Tag} {
+				  color: ${appConfig.theme.colors.neutrals['000']};
+				  font-size: 24px;
+				  font-weight: 600;
+			  }
+			  `}</style>
+	  </>
 	);
-}
+  }
 
-const Title = props => {
-	/* Functionalities */
-	const Tag = props.Tag;
-
+export default () => {
+	const defaultUser = "NathanMBR";
+	const [user, setUser] = useState(defaultUser);
+	const router = useRouter();
+  
 	return (
-		<>
-			{/* Elements */}
-			<Tag>
-				{props.children}
-			</Tag>
-
-			{/* Style */}
-			<style jsx>{`
-				${Tag} {
-					display: block;
-					padding: 5px;
-					text-align: center;
-					background-color: #222;
-					color: ${appConfig.theme.colors.neutrals["000"]};
-					border-radius: 5px;
-					box-shadow: 2px 2px 5px #333;
+	  <>
+		<Box
+		  styleSheet={{
+			display: 'flex', alignItems: 'center', justifyContent: 'center',
+			backgroundColor: appConfig.theme.colors.primary[500],
+			backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+			backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+		  }}
+		>
+		  <Box
+			styleSheet={{
+			  display: 'flex',
+			  alignItems: 'center',
+			  justifyContent: 'space-between',
+			  flexDirection: {
+				xs: 'column',
+				sm: 'row',
+			  },
+			  width: '100%', maxWidth: '700px',
+			  borderRadius: '5px', padding: '32px', margin: '16px',
+			  boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+			  backgroundColor: appConfig.theme.colors.neutrals[700],
+			}}
+		  >
+			{/* Formulário */}
+			<Box
+			  as="form"
+			  styleSheet={{
+				display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+				width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+			  }}
+			  onSubmit={event => {
+				event.preventDefault()
+				router.push("/chat")
+			  }}
+			>
+			  <Title tag="h2">Welcome!</Title>
+			  <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+				{appConfig.name}
+			  </Text>
+  
+			  <TextField
+				fullWidth
+				textFieldColors={{
+				  neutral: {
+					textColor: appConfig.theme.colors.neutrals[200],
+					mainColor: appConfig.theme.colors.neutrals[900],
+					mainColorHighlight: appConfig.theme.colors.primary[500],
+					backgroundColor: appConfig.theme.colors.neutrals[800],
+				  },
+				}}
+				placeholder={defaultUser}
+				onChange={
+					event =>
+						setUser(event.target.value || defaultUser)
 				}
-			`}</style>
-		</>
-	)
-}
-
-export default () => (
-	<div>
-		<GlobalStyle />
-		<Title Tag="h1">Hello, world! Welcome to React.js + Next.js!</Title>
-		<Title Tag="h2">This is a Title.</Title>
-		<h2>This isn't a Title (at least, not a Title component)</h2>
-	</div>
-);
+			  />
+			  <Button
+				type='submit'
+				label='Enter'
+				fullWidth
+				buttonColors={{
+				  contrastColor: appConfig.theme.colors.neutrals["000"],
+				  mainColor: appConfig.theme.colors.primary[500],
+				  mainColorLight: appConfig.theme.colors.primary[400],
+				  mainColorStrong: appConfig.theme.colors.primary[600],
+				}}
+			  />
+			</Box>
+			{/* Formulário */}
+  
+			{/* Photo Area */}
+			<Box
+			  styleSheet={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				maxWidth: '200px',
+				padding: '16px',
+				backgroundColor: appConfig.theme.colors.neutrals[800],
+				border: '1px solid',
+				borderColor: appConfig.theme.colors.neutrals[999],
+				borderRadius: '10px',
+				flex: 1,
+				minHeight: '240px',
+			  }}
+			>
+			  <Image
+				styleSheet={{
+				  borderRadius: '50%',
+				  marginBottom: '16px',
+				}}
+				src={`https://github.com/${user}.png`}
+			  />
+			  <Text
+				variant="body4"
+				styleSheet={{
+				  color: appConfig.theme.colors.neutrals[200],
+				  backgroundColor: appConfig.theme.colors.neutrals[900],
+				  padding: '3px 10px',
+				  borderRadius: '1000px'
+				}}
+			  >
+				{user}
+			  </Text>
+			</Box>
+			{/* Photo Area */}
+		  </Box>
+		</Box>
+	  </>
+	);
+  }
